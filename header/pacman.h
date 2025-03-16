71ed9b9aae0097a939684bc79557498ac736cc18
raihaniooo <raihanaaaz2@gmail.com>
2025-03-16 14:32:14 +0700

    fix pacman.h

--- header/pacman.h
+++ header/pacman.h
@@ -1,9 +1,6 @@
 #ifndef PACMAN_H
 #define PACMAN_H
-
 #include <graphics.h>
-
-typedef struct Ghost Ghost;  // Forward declaration
 
 typedef struct {
     int x, y;
@@ -13,11 +10,12 @@ typedef struct {
 } Pacman;
 
 void drawPacman(Pacman *p);
-void clearPacman(Pacman *p);
-void movePacman(Pacman *p, char key);
-int isColliding(Pacman *p, int newX, int newY);
 
-int checkCollisionWithGhost(Pacman *pacman, Ghost *ghost);
-void updatePacmanAfterCollision(Pacman *pacman, Ghost *ghost);
+void clearPacman(Pacman *p);
+
+void movePacman(Pacman *p, char key);
+
+
+int isColliding(Pacman *p, int newX, int newY);
 
 #endif
