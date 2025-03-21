df8a9f30a3a1adee8116eb8977867204e4181337
Faithh <keperluankuliah85@gmail.com>
2025-03-21 21:47:38 +0700

    powerup udah ditambahin cuma belom ada pengaruh ke ghost, coba cek collision sama ghostnya

--- header/powerup.h
+++ header/powerup.h
@@ -1,25 +1,27 @@
 #ifndef POWERUP_H
 #define POWERUP_H
 
-#include <graphics.h>
 #include <time.h>
 
-#define MAX_POWERUPS 3   // Jumlah Power-Up dalam game
-#define POWERUP_DURATION 10000  // 10 detik dalam milidetik
+#define MAX_POWERUPS 5
+#define POWERUP_DURATION 5000  // Durasi power-up dalam milidetik (5 detik)
 
 typedef struct {
-    int x, y;        // Posisi Power-Up
-    int active;      // 1 jika aktif, 0 jika sudah diambil
+    int x, y;
+    int active;
+    int type;  // 1 = Double Score, 2 = Kebal, 3 = Freeze
 } PowerUp;
 
+// deklarasi variabel global yg dipakedi powerup.c
 extern PowerUp powerUps[MAX_POWERUPS];
 extern int doublePointActive;
-extern clock_t doublePointTimer;
+extern int kebalActive;
+extern int freezeActive;
+extern clock_t doublePointTimer, kebalTimer, freezeTimer;
 
 void spawnPowerUps();
 void drawPowerUps();
 void checkPowerUpCollision(int pacmanX, int pacmanY);
 void updatePowerUpState();
-// int powerUpKosong(int i, int j);
 
 #endif
